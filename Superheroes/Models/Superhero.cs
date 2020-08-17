14a6f73ed94c6d81238b6738aa72ecbde4c978fb
Sam Clavette <samclavette@yahoo.com>
2020-08-17 10:31:32 -0500

    Initial commit. Created project. Added Superhero class along with its properties.

---
+++ Superheroes/Models/Superhero.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Superheroes.Models
+{
+    public class Superhero
+    {
+        [Key]
+
+        public string Name { get; set; }
+
+        public string AlterEgo { get; set; }
+
+        public string PrimaryAbility { get; set; }
+
+        public string SecondaryAbility { get; set; }
+
+        public string Catchphrase { get; set; }
+    }
+}
